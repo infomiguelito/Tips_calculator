426c4aa02524a2ded37d0439fe8c7a190818e27d
calculo-combustivel <miguelingoschaade@gmail.com>
2024-08-21 20:13:47 -0300

    app

--- app/src/main/java/com/example/tips_calculator/MainActivity.kt
+++ app/src/main/java/com/example/tips_calculator/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.tips_calculator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +15,14 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val btnclean = findViewById<Button>(R.id.btn_clean)
+        val tietotal = findViewById<TextInputEditText>(R.id.tie_total)
+        val tiepeople = findViewById<TextInputEditText>(R.id.tie_people)
+        val btnDone = findViewById<Button>(R.id.btn_calculator)
 
+        btnclean.setOnClickListener {
+
+        }
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
