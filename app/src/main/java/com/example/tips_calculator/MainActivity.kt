31a776ecf95f0665d60bf326c5491b8c4d94fa3b
calculo-combustivel <miguelingoschaade@gmail.com>
2024-08-22 21:26:30 -0300

    app

--- app/src/main/java/com/example/tips_calculator/MainActivity.kt
+++ app/src/main/java/com/example/tips_calculator/MainActivity.kt
@@ -1,27 +1,106 @@
 package com.example.tips_calculator
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tips_calculator.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btnclean = findViewById<Button>(R.id.btn_clean)
-        val tietotal = findViewById<TextInputEditText>(R.id.tie_total)
-        val tiepeople = findViewById<TextInputEditText>(R.id.tie_people)
-        val btnDone = findViewById<Button>(R.id.btn_calculator)
+        var percentage: Int = 0
+        binding.rbOptionsOne.setOnCheckedChangeListener { _, isCheked ->
+            println("miguel one $isCheked")
+            if (isCheked) {
+                percentage = 10
+            }
+        }
 
-        btnclean.setOnClickListener {
+        binding.rbOptionsTwo.setOnCheckedChangeListener { _, isCheked ->
+            println("miguel two $isCheked")
+            if (isCheked) {
+                percentage = 15
+            }
+        }
 
+        binding.rbOptionsThree.setOnCheckedChangeListener { _, isCheked ->
+            println("miguel three $isCheked")
+            if (isCheked) {
+                percentage = 20
+            }
+        }
+        var numbOfPeopleSelect = 0
+        binding.spinerPeople.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(
+                p0: AdapterView<*>?,
+                p1: View?,
+                p2: Int,
+                p3: Long
+            ) {
+                numbOfPeopleSelect = p2
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.numpeople,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinerPeople.adapter = adapter
+
+
+        binding.btnCalculator.setOnClickListener {
+
+
+            val totalTemp = binding.tieTotal.text
+
+            if (totalTemp?.isEmpty() == true) {
+                Snackbar.make(binding.tieTotal, "Preencha todos os campos", Snackbar.LENGTH_LONG)
+                    .show()
+            } else {
+                val total: Float = totalTemp.toString().toFloat()
+                val people: Int = numbOfPeopleSelect
+
+                val Temp = total / people
+                val tips = Temp * percentage / 100
+                val totaltips = Temp + tips
+                binding.tvResult.text = "Total: $totaltips"
+            }
+
+
+        }
+
+
+        binding.btnClean.setOnClickListener {
+
+            binding.tvResult.text = ""
+            binding.tieTotal.setText("")
+            binding.rbOptionsOne.isChecked = false
+            binding.rbOptionsTwo.isChecked = false
+            binding.rbOptionsThree.isChecked = false
         }
 
 
@@ -32,3 +111,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+
+
